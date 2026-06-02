a644f315421f1650517dda49dbe178a81c3d6985
locngodev <62205ngovanloc@gmail.com>
2026-06-03 01:51:12 +0700

    Fix whitenoise strict manifest error

--- studybud/settings.py
+++ studybud/settings.py
@@ -174,6 +174,7 @@ if os.environ.get('CLOUDINARY_URL'):
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
