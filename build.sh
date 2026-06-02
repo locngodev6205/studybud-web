4f50a1707d8cc72b1e5545077e8816ce328cad04
locngodev <62205ngovanloc@gmail.com>
2026-06-03 01:21:33 +0700

    Auto create superuser via env

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('ADMIN_USERNAME')
+password = os.environ.get('ADMIN_PASSWORD')
+email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser {username} created automatically!')
+"
